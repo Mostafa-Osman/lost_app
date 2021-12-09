d87c089e3209f4ac3c465c61d4afcdedc60dcf36
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2021-12-10 01:00:20 +0200

    update splash, home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:lost_app/home/bottom_navigation_bar.dart';
 
 import 'modules/login/forget_password.dart';
 import 'modules/login/login.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       supportedLocales: [
         Locale('ar', ''),
       ],
-      home: OnBoardingScreen(),
+      home: HomeScreen(),
     );
   }
 }
