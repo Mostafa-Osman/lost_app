bd68a613303bc9248c89713b9df299389be17686
mosta <Mostafaosman225@gmail.com>
2021-12-27 18:47:46 +0200

    no commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:lost_app/modules/profile/ui/edit_profile.dart';
+import 'package:lost_app/modules/profile/profile_cubit/cubit.dart';
 import 'package:lost_app/modules/search/search_cubit/cubit.dart';
 import 'package:lost_app/modules/setting/setting_cubit/cubit.dart';
+import 'package:lost_app/modules/splash/splash_screen.dart';
+import 'package:lost_app/modules/verify_mobile/verify_cubit/cubit.dart';
 import 'modules/add_person_data/add_person_cubit/cubit.dart';
 import 'modules/login/login_cubit/cubit.dart';
 import 'modules/on_boarding/on_boarding_cubit/cubit.dart';
-import 'modules/profile/profile_cubit/cubit.dart';
 import 'modules/register/register_cubit/cubit.dart';
 import 'modules/verify_mobile/ui/Verify_mobile.dart';
 
@@ -25,7 +26,10 @@ class MyApp extends StatelessWidget {
           BlocProvider(create: (context) => OnBoardingCubit()),
           BlocProvider(create: (context) => SearchCubit()),
           BlocProvider(create: (context) => SettingCubit()),
+          BlocProvider(create: (context) => VerifyCubit()),
           BlocProvider(create: (context) => ProfileCubit()),
+
+
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
@@ -38,7 +42,7 @@ class MyApp extends StatelessWidget {
           supportedLocales: [
             Locale('ar', ''),
           ],
-          home: EditProfile(),
+          home: SplashScreen(),
         ));
   }
 }
